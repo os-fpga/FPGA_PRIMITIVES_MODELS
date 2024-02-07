ea5f6fbef006043375e926072eb69d6fefbac206
NadeemYaseen <pu.engineerg@gmail.com>
2024-02-07 09:30:50 +0000

    Added SIM change files from 1.0.4beta

--- sim_models_internal/verilog/TDP_RAM36K.v
+++ sim_models_internal/verilog/TDP_RAM36K.v
@@ -263,7 +263,7 @@ module TDP_RAM36K #(
     if (RAM_ADDR_WIDTH == A_WRITE_ADDR_WIDTH)
       find_a_write_index = 0;
     else    
-      find_a_write_index = ADDR_A[14-RAM_ADDR_WIDTH:15-A_WRITE_ADDR_WIDTH]; 
+      find_a_write_index = ADDR_A[15-A_WRITE_ADDR_WIDTH:14-RAM_ADDR_WIDTH]; 
 
   endfunction
 
@@ -273,7 +273,7 @@ module TDP_RAM36K #(
     if (RAM_ADDR_WIDTH == A_READ_ADDR_WIDTH)
       find_a_read_index = 0;
     else    
-      find_a_read_index = ADDR_A[14-RAM_ADDR_WIDTH:15-A_READ_ADDR_WIDTH]; 
+      find_a_read_index = ADDR_A[15-A_READ_ADDR_WIDTH:14-RAM_ADDR_WIDTH]; 
 
   endfunction
 
@@ -283,7 +283,7 @@ module TDP_RAM36K #(
     if (RAM_ADDR_WIDTH == B_WRITE_ADDR_WIDTH)
       find_b_write_index = 0;
     else    
-      find_b_write_index = ADDR_B[14-RAM_ADDR_WIDTH:15-B_WRITE_ADDR_WIDTH]; 
+      find_b_write_index = ADDR_B[15-B_WRITE_ADDR_WIDTH:14-RAM_ADDR_WIDTH]; 
 
   endfunction
 
@@ -293,7 +293,7 @@ module TDP_RAM36K #(
     if (RAM_ADDR_WIDTH == B_READ_ADDR_WIDTH)
       find_b_read_index = 0;
     else    
-      find_b_read_index = ADDR_B[14-RAM_ADDR_WIDTH:15-B_READ_ADDR_WIDTH]; 
+      find_b_read_index = ADDR_B[15-B_READ_ADDR_WIDTH:14-RAM_ADDR_WIDTH]; 
 
   endfunction
 
@@ -345,7 +345,8 @@ module TDP_RAM36K #(
 
   initial
     $timeformat(-9,0," ns", 5);
- initial begin
+
+     initial begin
 
     if ((WRITE_WIDTH_A < 1) || (WRITE_WIDTH_A > 36)) begin
        $display("TDP_RAM36K instance %m WRITE_WIDTH_A set to incorrect value, %d.  Values must be between 1 and 36.", WRITE_WIDTH_A);
