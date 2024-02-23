f446e6472ac9506112e201891a360ba8d314a366
alaindargelas <alain.dargelas@gmail.com>
2024-02-23 10:59:01 -0800

    Remove redundant bmux model

--- sim_models/verilog/LUT1.v
+++ sim_models/verilog/LUT1.v
@@ -20,30 +20,3 @@ module LUT1 #(
 
 endmodule
 `endcelldefine
-
-`celldefine
-module \$bmux (A, S, Y);
-
-parameter WIDTH = 0;
-parameter S_WIDTH = 0;
-
-input [(WIDTH << S_WIDTH)-1:0] A;
-input [S_WIDTH-1:0] S;
-output [WIDTH-1:0] Y;
-
-wire [WIDTH-1:0] bm0_out, bm1_out;
-
-generate
-	if (S_WIDTH > 1) begin:muxlogic
-		\$bmux #(.WIDTH(WIDTH), .S_WIDTH(S_WIDTH-1)) bm0 (.A(A), .S(S[S_WIDTH-2:0]), .Y(bm0_out));
-		\$bmux #(.WIDTH(WIDTH), .S_WIDTH(S_WIDTH-1)) bm1 (.A(A[(WIDTH << S_WIDTH)-1:WIDTH << (S_WIDTH - 1)]), .S(S[S_WIDTH-2:0]), .Y(bm1_out));
-		assign Y = S[S_WIDTH-1] ? bm1_out : bm0_out;
-	end else if (S_WIDTH == 1) begin:simple
-		assign Y = S ? A[1] : A[0];
-	end else begin:passthru
-		assign Y = A;
-	end
-endgenerate
-
-endmodule
-`endcelldefine
