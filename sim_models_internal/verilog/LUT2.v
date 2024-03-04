90e8f705a009fec8c4f926be916e70655aad6961
NadeemYaseen <pu.engineerg@gmail.com>
2024-03-04 06:53:05 +0000

    Added SIM change files from 1.0.8

--- sim_models_internal/verilog/LUT2.v
+++ sim_models_internal/verilog/LUT2.v
@@ -14,10 +14,8 @@ module LUT2 #(
   output Y // Data Output
 );
 
-  //assign Y = INIT_VALUE[A] ;
-  \$bmux #(.WIDTH(1), .S_WIDTH(2)) mux(.A(INIT_VALUE), .S(A), .Y(Y));
+  assign Y = INIT_VALUE[A] ;
 
 
 endmodule
 `endcelldefine
-
