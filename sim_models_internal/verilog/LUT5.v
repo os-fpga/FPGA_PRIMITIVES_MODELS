3b2b47b19d5bd59b33e90238341d3dffd283960c
NadeemYaseen <pu.engineerg@gmail.com>
2024-03-15 15:20:52 +0000

    Added SIM change files from 1.0.9

--- sim_models_internal/verilog/LUT5.v
+++ sim_models_internal/verilog/LUT5.v
@@ -14,10 +14,12 @@ module LUT5 #(
   output Y // Data Output
 );
 
-  //assign Y = INIT_VALUE[A] ;
-  \$bmux #(.WIDTH(1), .S_WIDTH(5)) mux(.A(INIT_VALUE), .S(A), .Y(Y));
+  wire [15: 0] s4 = A[4] ? INIT_VALUE[31:16] : INIT_VALUE[15: 0];
+  wire [ 7: 0] s3 = A[3] ?   s4[15: 8] :   s4[ 7: 0];
+  wire [ 3: 0] s2 = A[2] ?   s3[ 7: 4] :   s3[ 3: 0];
+  wire [ 1: 0] s1 = A[1] ?   s2[ 3: 2] :   s2[ 1: 0];
+  assign Y = A[0] ? s1[1] : s1[0];
 
 
 endmodule
 `endcelldefine
-
