eff241ecc88f793d7579b1361097abdfbedfa5d9
moinijaz <moin.ijaz9@gmail.com>
2024-06-13 12:46:16 +0500

    I_DDR tb updated

--- tb/I_DDR/I_DDR_tb.v
+++ tb/I_DDR/I_DDR_tb.v
@@ -2,26 +2,30 @@ module I_DDR_tb;
 
   // Ports
   reg CLK = 0;
-  wire[1:0] O1, O2;
+  wire[1:0] I_DDR_Q, Expected_Q;
   reg R;
   reg DD;
   integer mismatch = 0;
+  integer i;
 
   always #5 CLK = ~CLK;
 
-  always @(CLK) begin
-    DD <= $random;
-    compare(O1, O2);
-  end
-
   initial begin
     begin
-      // DD = 0;
+      DD = 0;
       $display ("Random input ");  
       R = 0;
-      repeat(2) @(posedge CLK);
+      @(posedge CLK);
       R = 1;
-      repeat(10) @(posedge CLK);
+      @(posedge CLK);
+
+      for (i = 0; i < 20; i = i + 1) begin
+        @(CLK);
+        DD <= $random;
+        compare(I_DDR_Q, Expected_Q);
+      end
+      repeat(2) @(posedge CLK);
+
       if (mismatch > 0) begin
       $display("-----------------Simulation Failed-----------------");
       end
@@ -39,13 +43,12 @@ module I_DDR_tb;
       $finish;
   end
 
-  I_DDR 
-  I_DDR_dut1 (
-    .D (DD),
-    .R (R),
-    .E (1'b1),
-    .C (CLK),
-    .Q (O1)
+  I_DDR I_DDR_dut1 (
+    .D(DD),
+    .R(R),
+    .E(1'b1),
+    .C(CLK),
+    .Q(I_DDR_Q)
   );
 
   i_ddr i_ddr_dut2 (
@@ -53,12 +56,12 @@ module I_DDR_tb;
     .rst(R),
     .en(1'b1),
     .clk(CLK),
-    .Q(O2)
+    .Q(Expected_Q)
   );
 
-task compare(O1, O2);
-    if(O1 !== O2) begin
-        $display("Output mismatch. dut1: %0h, dut2: %0h", O1, O2);
+task compare(I_DDR_Q, Expected_Q);
+    if(I_DDR_Q !== Expected_Q) begin
+        $display("Output mismatch. I_DDR_Q: %0h, Expected_Q: %0h", I_DDR_Q, Expected_Q);
         mismatch = mismatch + 1;
     end
 endtask
@@ -66,28 +69,37 @@ endtask
 endmodule
 
 module i_ddr (
-  input data, input rst, input en, input clk, output [1:0] Q
+  input data, 
+  input rst, 
+  input en, 
+  input clk, 
+  output reg [1:0] Q
 );
-reg [1:0] Q = 2'd0;
-always@(posedge clk) begin
-  if (en) begin
-    Q[0] <= data;
+
+reg [1:0] temp;
+
+always@(posedge clk or negedge rst) begin
+  if (!rst)
+    temp[0] <= 1'd0;
+  else if (en) begin
+    temp[0] <= data;
   end 
 end
 
-always@(clk) begin
-  if(!rst)
-    Q <= 2'b00;
-end
-
-always@(negedge clk) begin
-  if (en) begin
-    Q[1] <= data;
+always@(negedge clk or negedge rst) begin
+  if (!rst)
+    temp[1] <= 1'd0;
+  else if (en) begin
+    temp[1] <= data;
   end
 end
 
-always@(negedge rst) begin
-  Q <= 2'b00;
+always @(posedge clk or negedge rst) begin
+  if (!rst)
+    Q <= 2'd0;
+  else
+    Q[0] = temp[1];
+    Q[1] = temp[0];
 end
 
 endmodule
