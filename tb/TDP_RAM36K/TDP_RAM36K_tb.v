d679c10160124bd63edd783ec3c0ff55e1284d25
moinijaz <moin.ijaz9@gmail.com>
2024-06-25 09:56:04 +0500

    TDP_RAM36K tb updated

--- tb/TDP_RAM36K/TDP_RAM36K_tb.v
+++ tb/TDP_RAM36K/TDP_RAM36K_tb.v
@@ -1,3 +1,5 @@
+`timescale 1ns/1ps
+`celldefine
 // Self-Checking Testbench for TDP_RAM36K simulation model
 // Testbench Not modeled for ASymmetric RAM
 // So, Keep the Write/Read Width Same for Both Ports
@@ -23,8 +25,12 @@ module TDP_RAM36K_tb();
 	wire [READ_WIDTH_B-1:0] RDATA_B; // Read data port B
 	wire [3:0] RPARITY_B; // Read parity port B
 
+	/* verilator lint_off WIDTHCONCAT */
 	parameter [32767:0] INIT = {32768{1'b0}}; // Initial Contents of memory
+	/* verilator lint_on WIDTHCONCAT */
+	/* verilator lint_off WIDTHCONCAT */
   parameter [4095:0] INIT_PARITY = {4096{1'b0}}; // Initial Contents of memory
+	/* verilator lint_on WIDTHCONCAT */
   parameter WRITE_WIDTH_A = 36; // Write data width on port A (1-36)
   parameter READ_WIDTH_A = 	36; // Read data width on port A (1-36)
   parameter WRITE_WIDTH_B = 36; // Write data width on port B (1-36)
@@ -63,7 +69,9 @@ module TDP_RAM36K_tb();
 	reg [RAM_ADDR_WIDTH-1:0] temp_ram_addr;
 
 	// Parity Ram
+	/* verilator lint_off LITENDIAN */
 	reg [RAM_PARITY_WIDTH-1:0] local_parity_ram [2**RAM_ADDR_WIDTH-1:0];
+	/* verilator lint_on LITENDIAN */
 
 	integer f_p, g_p, h_p;
 	integer f, g, h, i, j, k, m;
@@ -73,7 +81,11 @@ module TDP_RAM36K_tb();
 		f_p = 0;
 		for (g_p = 0; g_p < 2**RAM_ADDR_WIDTH; g_p = g_p + 1)
 			for (h_p = 0; h_p < RAM_PARITY_WIDTH; h_p = h_p + 1) begin
-				local_parity_ram[g_p][h_p] <= INIT_PARITY[f_p];
+				`ifdef SIM_VERILATOR
+					local_parity_ram[g_p][h_p] = INIT_PARITY[f_p];
+				`else
+					local_parity_ram[g_p][h_p] <= INIT_PARITY[f_p];
+				`endif
 				f_p = f_p + 1;
 			end
 	end
@@ -83,14 +95,18 @@ module TDP_RAM36K_tb();
 			f = 0;
 			for (g = 0; g < 2**RAM_ADDR_WIDTH; g = g + 1)
 				for (h = 0; h < RAM_DATA_WIDTH; h = h + 1) begin
-					local_ram[g][h] <= INIT[f];
+					`ifdef SIM_VERILATOR
+						local_ram[g][h] = INIT[f];
+					`else
+						local_ram[g][h] <= INIT[f];
+					`endif
 					f = f + 1;
 				end
 		end
 
 	integer error=0;
 	// debug flag
-  bit debug=0;
+  bit debug=1;
 
 	//Clock Generation//
 initial begin
@@ -124,51 +140,77 @@ tdp_ram36k_inst
 		.BE_B(BE_B), // Byte-write enable port B
 		.ADDR_A(ADDR_A), // Address port A
 		.ADDR_B(ADDR_B), // Address port B
+		/* verilator lint_off WIDTH */
 		.WDATA_A(WDATA_A), // Write data port A
+		/* verilator lint_on WIDTH */
 		.WPARITY_A(WPARITY_A), // Write parity port A
+		/* verilator lint_off WIDTH */
 		.WDATA_B(WDATA_B), // Write data port B
+		/* verilator lint_on WIDTH */
 		.WPARITY_B(WPARITY_B), // Write parity port B
+		/* verilator lint_off WIDTH */
 		.RDATA_A(RDATA_A), // Read data port A
+		/* verilator lint_on WIDTH */
 		.RPARITY_A(RPARITY_A), // Read parity port A
+		/* verilator lint_off WIDTH */
 		.RDATA_B(RDATA_B), // Read data port B
+		/* verilator lint_on WIDTH */
 		.RPARITY_B(RPARITY_B) // Read parity port B
 	);
 
 `ifdef VCD
 	initial begin
-		$dumpfile("TDP_RAM36K.vcd");
+		$dumpfile("wave.vcd");
 		$dumpvars;
 	end
 `endif
 
 	initial begin
 		// Corner Cases for RAM1
+		/* verilator lint_off WIDTH */
 		directed_read_or_write('0,'0,'0,'0,1,0); // 0 on 0 - write - port1A
 		directed_read_or_write('1,'1,'0,'0,1,1); // 0 on 1 - write - port1B
 		directed_read_or_write('0,'0,'0,'0,0,0); // 0 on 0 - read - port1A
 		directed_read_or_write('1,'1,'0,'0,0,1); // 0 on 1 - read -port1B
-		// // Write Port1A
+		// // // Write Port1A
 		directed_read_or_write('0,'0,'hdead,'0,1,0); // AddrA, AddrB, DataA, DataB, Write, PortAB
 		directed_read_or_write('h5,'1,'1,'0,1,0); // AddrA, AddrB, DataA, DataB, Write, PortAB
-		// Read Port1A
+		// // Read Port1A
 		directed_read_or_write('0,'0,'hdead,'0,0,0); // AddrA, AddrB, DataA, DataB, Write, PortAB
 		directed_read_or_write('h5,'1,'1,'0,0,0); // AddrA, AddrB, DataA, DataB, Write, PortAB
-		// Write Port1B
+		// // Write Port1B
 		directed_read_or_write('0,'0,'hbeef,'hbeef,1,1); // AddrA, AddrB, DataA, DataB, Write, PortAB
 		directed_read_or_write('h5,'h5,'1,'1,1,1); // AddrA, AddrB, DataA, DataB, Write, PortAB
 		// Read Port1B
 		directed_read_or_write('0,'0,'hbeef,'hbeef,0,1); // AddrA, AddrB, DataA, DataB, Write, PortAB
 		directed_read_or_write('h5,'h5,'1,'1,0,1); // AddrA, AddrB, DataA, DataB, Write, PortAB
-		// Read after Write (Random)
+		// // Read after Write (Random)
 		repeat (255) begin
+			/* verilator lint_off WIDTH */
 			temp_ram_data_a = $urandom_range(0, (2**RAM_DATA_WIDTH)-1);
 			temp_ram_data_b = $urandom_range(0, (2**RAM_DATA_WIDTH)-1);
+			/* verilator lint_on WIDTH */
+			/* verilator lint_off WIDTH */
 			temp_ram_port = $urandom_range(0, 1);
+			// temp_ram_port = 0;
+			/* verilator lint_on WIDTH */
+			/* verilator lint_off WIDTH */
 			temp_ram_addr = $urandom_range(0, (2**RAM_ADDR_WIDTH)-1);
+			/* verilator lint_on WIDTH */
 			directed_read_or_write(temp_ram_addr,temp_ram_addr,temp_ram_data_a,temp_ram_data_b,1,temp_ram_port); // AddrA, AddrB, DataA, DataB, Write, PortAB
 			directed_read_or_write(temp_ram_addr,temp_ram_addr,temp_ram_data_a,temp_ram_data_b,0,temp_ram_port); // AddrA, AddrB, DataA, DataB, Write, PortAB
+			/* verilator lint_on WIDTH */
 		end
 
+		// // Collision Check
+		// /* verilator lint_off WIDTH */
+		// @(negedge CLK_A);
+		// ADDR_A = 'h0;
+		// WEN_A = 1; WDATA_A = 'hFFFF;
+		// ADDR_B = 'h0;
+		// REN_B = 1; WDATA_B = 'hFFFF;
+		// /* verilator lint_on WIDTH */
+	  
 		test_status(error);
 		#100;
 		$finish();
@@ -180,16 +222,30 @@ tdp_ram36k_inst
       if(portAB==1'b0)
         begin
           @(negedge CLK_A);
-          WEN_A = 1; WDATA_A = d_dinA; drive_addr(d_addrA, RAM_ADDR_WIDTH, portAB); WPARITY_A = $urandom_range(0, (2**RAM_PARITY_WIDTH)-1); BE_A = $urandom_range(0, 15);
+					/* verilator lint_off WIDTH */
+					/* verilator lint_off IGNOREDRETURN */
+          WEN_A = 1; WDATA_A = d_dinA; drive_addr(d_addrA, RAM_ADDR_WIDTH, portAB); WPARITY_A = $urandom_range(0, (2**RAM_PARITY_WIDTH)-1); //BE_A = 4'b1111; 
+					BE_A = $urandom_range(0, 15);
+					/* verilator lint_on WIDTH */
+					/* verilator lint_on IGNOREDRETURN */
+					/* verilator lint_off WIDTH */
           @(posedge CLK_A) local_ram[d_addrA] = RAM_Data_wrt_BE(d_addrA, d_dinA, WPARITY_A, BE_A);
+					/* verilator lint_on WIDTH */
           @(negedge CLK_A);
           WEN_A = 0;
         end
       else if(portAB==1'b1)
         begin
           @(negedge CLK_B);
-          WEN_B = 1; WDATA_B = d_dinB; drive_addr(d_addrB, RAM_ADDR_WIDTH, portAB); WPARITY_B = $urandom_range(0, (2**RAM_PARITY_WIDTH)-1); BE_B = $urandom_range(0, 15);
+					/* verilator lint_off WIDTH */
+					/* verilator lint_off IGNOREDRETURN */
+          WEN_B = 1; WDATA_B = d_dinB; drive_addr(d_addrB, RAM_ADDR_WIDTH, portAB); WPARITY_B = $urandom_range(0, (2**RAM_PARITY_WIDTH)-1); //BE_B = 4'b1111;
+					BE_B = $urandom_range(0, 15);
+					/* verilator lint_on WIDTH */
+					/* verilator lint_on IGNOREDRETURN */
+					/* verilator lint_off WIDTH */
           @(posedge CLK_B) local_ram[d_addrB] = RAM_Data_wrt_BE(d_addrB, d_dinB, WPARITY_B, BE_B);
+					/* verilator lint_on WIDTH */
           @(negedge CLK_B);
           WEN_B = 0;
         end
@@ -197,19 +253,37 @@ tdp_ram36k_inst
     else begin
       if(portAB==1'b0)
         begin
+					/* verilator lint_off WIDTH */
+					/* verilator lint_off IGNOREDRETURN */
           @(negedge CLK_A);  WEN_A = 0; drive_addr(d_addrA, RAM_ADDR_WIDTH, portAB);
+					/* verilator lint_on WIDTH */
+					/* verilator lint_on IGNOREDRETURN */
+					/* verilator lint_off WIDTH */
           @(negedge CLK_A);	REN_A = 1;
+					/* verilator lint_on WIDTH */
           @(negedge CLK_A);	REN_A = 0;
+					/* verilator lint_off WIDTH */
           compare(RDATA_A, local_ram[d_addrA], d_addrA, 0);
+					/* verilator lint_on WIDTH */
+					/* verilator lint_off WIDTH */
           compare(RPARITY_A, local_parity_ram[d_addrA], d_addrA, 1);
+					/* verilator lint_on WIDTH */
         end
       else if(portAB==1'b1)
         begin
+					/* verilator lint_off WIDTH */
+					/* verilator lint_off IGNOREDRETURN */
           @(negedge CLK_B);   WEN_B = 0; drive_addr(d_addrB, RAM_ADDR_WIDTH, portAB);
+					/* verilator lint_on WIDTH */
+					/* verilator lint_on IGNOREDRETURN */
           @(negedge CLK_B);	 REN_B = 1;
           @(negedge CLK_B);	 REN_B = 0;
+					/* verilator lint_off WIDTH */
           compare(RDATA_B, local_ram[d_addrB], d_addrB, 0);
+					/* verilator lint_on WIDTH */
+					/* verilator lint_off WIDTH */
           compare(RPARITY_B, local_parity_ram[d_addrB], d_addrB, 1);
+					/* verilator lint_on WIDTH */
         end
     end
 
@@ -264,53 +338,83 @@ endfunction
 function integer drive_addr(input reg [14:0] addr, input integer addr_width, input reg [0:0] portAB);
     if (addr_width == 10) begin
 			if(portAB==1'b0)
+				/* verilator lint_off WIDTH */
 				ADDR_A[14:5] = addr;
+				/* verilator lint_on WIDTH */
 			else if(portAB==1'b1)
+				/* verilator lint_off WIDTH */
 				ADDR_B[14:5] = addr;
+				/* verilator lint_on WIDTH */
 		end
     else if (addr_width == 11) begin
 			if(portAB==1'b0)
+				/* verilator lint_off WIDTH */
 				ADDR_A[14:4] = addr;
+				/* verilator lint_on WIDTH */
 			else if(portAB==1'b1)
+				/* verilator lint_off WIDTH */
 				ADDR_B[14:4] = addr;
+				/* verilator lint_on WIDTH */
 		end
     else if (addr_width == 12) begin
 			if(portAB==1'b0)
+				/* verilator lint_off WIDTH */
 				ADDR_A[14:3] = addr;
+				/* verilator lint_on WIDTH */
 			else if(portAB==1'b1)
+				/* verilator lint_off WIDTH */
 				ADDR_B[14:3] = addr;
+				/* verilator lint_on WIDTH */
 		end
     else if (addr_width == 13) begin
 			if(portAB==1'b0)
+				/* verilator lint_off WIDTH */
 				ADDR_A[14:2] = addr;
+				/* verilator lint_on WIDTH */
 			else if(portAB==1'b1)
+				/* verilator lint_off WIDTH */
 				ADDR_B[14:2] = addr;
+				/* verilator lint_on WIDTH */
 		end
     else if (addr_width == 14) begin
 			if(portAB==1'b0)
+				/* verilator lint_off WIDTH */
 				ADDR_A[14:1] = addr;
+				/* verilator lint_on WIDTH */
 			else if(portAB==1'b1)
+				/* verilator lint_off WIDTH */
 				ADDR_B[14:1] = addr;
+				/* verilator lint_on WIDTH */
 		end
 		else if (addr_width == 15) begin
 			if(portAB==1'b0)
+				/* verilator lint_off WIDTH */
 				ADDR_A[14:0] = addr;
+				/* verilator lint_on WIDTH */
 			else if(portAB==1'b1)
+				/* verilator lint_off WIDTH */
 				ADDR_B[14:0] = addr;
+				/* verilator lint_on WIDTH */
 		end
     else begin
       if(portAB==1'b0)
+				/* verilator lint_off WIDTH */
 				ADDR_A[14:0] = 0;
+				/* verilator lint_on WIDTH */
 			else if(portAB==1'b1)
+				/* verilator lint_off WIDTH */
 				ADDR_B[14:0] = 0;
+				/* verilator lint_on WIDTH */
 			end
   endfunction
-
+	/* verilator lint_off LITENDIAN */
 	function logic [RAM_DATA_WIDTH-1:0] RAM_Data_wrt_BE(input reg [RAM_ADDR_WIDTH-1:0] addr, input reg [RAM_DATA_WIDTH-1:0] din, input reg [RAM_PARITY_WIDTH-1:0] parity, input reg [3:0] BE);
-    logic [RAM_DATA_WIDTH-1:0] dout;
+    /* verilator lint_on LITENDIAN */
+		logic [RAM_DATA_WIDTH-1:0] dout;
     if (RAM_DATA_WIDTH > 9) begin
 			case (BE)
 				4'b0000: dout = local_ram[addr];
+				/* verilator lint_off SELRANGE */
 				4'b0001: begin
 					local_ram[addr][7:0] = din[7:0];
 					local_parity_ram[addr][0] = parity[0];
@@ -427,6 +531,7 @@ function integer drive_addr(input reg [14:0] addr, input integer addr_width, inp
 						local_parity_ram[addr][3:2] = parity[3:2];
 					end
 					dout = local_ram[addr];
+					/* verilator lint_on SELRANGE */
 				end
 			endcase
 		end
@@ -471,7 +576,7 @@ begin
 			$display("");
 			$display("");	
 			$display("----------------------------------------------");
-      		$display("                 Test Passed                  ");
+      $display("                 TEST_PASSED                  ");
 			$display("----------------------------------------------");
     end
     else   
@@ -501,7 +606,7 @@ begin
 			$display("");
 			$display("");	
       $display("----------------------------------------------");
-      $display("                 Test Failed                  ");
+      $display("                 TEST_FAILED                  ");
       $display("----------------------------------------------");
     end
 end
