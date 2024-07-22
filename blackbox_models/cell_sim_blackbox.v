934ac5350be44adf3e550318bb0bb8a9049af5ca
NadeemYaseen <pu.engineerg@gmail.com>
2024-07-22 22:54:06 +0000

    Added SIM change files from 1.4.6

--- blackbox_models/cell_sim_blackbox.v
+++ blackbox_models/cell_sim_blackbox.v
@@ -7,7 +7,7 @@
 `celldefine
 (* blackbox *)
 module BOOT_CLOCK #(
-  parameter PERIOD = 25.0 // Clock period for simulation purposes (nS)
+  parameter PERIOD = 25 // Clock period for simulation purposes (nS)
   ) (
   output reg O
 );
@@ -351,6 +351,20 @@ module I_DELAY #(
 endmodule
 `endcelldefine
 //
+// I_FAB black box model
+// Marker Buffer for periphery to fabric transition
+//
+// Copyright (c) 2024 Rapid Silicon, Inc.  All rights reserved.
+//
+`celldefine
+(* blackbox *)
+module I_FAB (
+  input logic I,
+  output logic O
+);
+endmodule
+`endcelldefine
+//
 // I_SERDES black box model
 // Input Serial Deserializer
 //
@@ -605,6 +619,20 @@ module O_DELAY #(
   output logic [5:0] DLY_TAP_VALUE,
   (* clkbuf_sink *)
   input logic CLK_IN,
+  output logic O
+);
+endmodule
+`endcelldefine
+//
+// O_FAB black box model
+// Marker Buffer for fabric to periphery transition
+//
+// Copyright (c) 2024 Rapid Silicon, Inc.  All rights reserved.
+//
+`celldefine
+(* blackbox *)
+module O_FAB (
+  input logic I,
   output logic O
 );
 endmodule
@@ -946,7 +974,9 @@ module TDP_RAM18KX2 #(
   input logic WEN_B2,
   input logic REN_A2,
   input logic REN_B2,
+  (* clkbuf_sink *)
   input logic CLK_A2,
+  (* clkbuf_sink *)
   input logic CLK_B2,
   input logic [1:0] BE_A2,
   input logic [1:0] BE_B2,
@@ -1001,243 +1031,4 @@ module TDP_RAM36K #(
   output reg [3:0] RPARITY_B
 );
 endmodule
-`endcelldefine
-
-
-//------------------------------------------------------------------------------
-//
-// Copyright (C) 2023 RapidSilicon
-//
-// genesis3 LATChes
-//
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-// Positive level-sensitive latch implemented with feed-back loop LUT
-//------------------------------------------------------------------------------
-
-`celldefine
-(* blackbox *)
-module LATCH(D, G, Q);
-  input D;
-  input G;
-  output Q;
-
-endmodule
-`endcelldefine
-
-//------------------------------------------------------------------------------
-// Negative level-sensitive latch implemented with feed-back loop LUT
-//------------------------------------------------------------------------------
-`celldefine
-(* blackbox *)
-module LATCHN(D, G, Q);
-  input D;
-  input G;
-  output Q;
-
-endmodule
-`endcelldefine
-
-//------------------------------------------------------------------------------
-// Positive level-sensitive latch with active-high asyncronous reset
-// implemented with feed-back loop LUT
-//------------------------------------------------------------------------------
-`celldefine
-(* blackbox *)
-module LATCHR(D, G, R, Q);
-  input D;
-  input G;
-  output Q;
-  input R;
-
-endmodule
-`endcelldefine
-//------------------------------------------------------------------------------
-// Positive level-sensitive latch with active-high asyncronous set
-// implemented with feed-back loop LUT
-//------------------------------------------------------------------------------
-`celldefine
-(* blackbox *)
-module LATCHS(D, G, R, Q);
-  input D;
-  input G;
-  output Q;
-  input R;
-
-endmodule
-`endcelldefine
-
-//------------------------------------------------------------------------------
-// Negative level-sensitive latch with active-high asyncronous reset
-// implemented with feed-back loop LUT
-//------------------------------------------------------------------------------
-`celldefine
-(* blackbox *)
-module LATCHNR(D, G, R, Q);
-  input D;
-  input G;
-  output Q;
-  input R;
-
-endmodule
-`endcelldefine
-
-//------------------------------------------------------------------------------
-// Negative level-sensitive latch with active-high asyncronous set
-// implemented with feed-back loop LUT
-//------------------------------------------------------------------------------
-`celldefine
-(* blackbox *)
-module LATCHNS(D, G, R, Q);
-  input D;
-  input G;
-  output Q;
-  input R;
-
-endmodule
-`endcelldefine
-
-`celldefine
-(* blackbox *)
-module BRAM2x18_TDP (A1ADDR, A1DATA, A1EN, B1ADDR, B1DATA, B1EN, C1ADDR, C1DATA, C1EN, CLK1, CLK2, CLK3, CLK4, D1ADDR, D1DATA, D1EN, E1ADDR, E1DATA, E1EN, F1ADDR, F1DATA, F1EN, G1ADDR, G1DATA, G1EN, H1ADDR, H1DATA, H1EN);
-    parameter CFG_ABITS = 11;
-    parameter CFG_DBITS = 18;
-    parameter CFG_ENABLE_B = 4;
-    parameter CFG_ENABLE_D = 4;
-    parameter CFG_ENABLE_F = 4;
-    parameter CFG_ENABLE_H = 4;
-
-    parameter CLKPOL2 = 1;
-    parameter CLKPOL3 = 1;
-    parameter [18431:0] INIT0 = 18432'bx;
-    parameter [18431:0] INIT1 = 18432'bx;
-
-    input CLK1;
-    input CLK2;
-    input CLK3;
-    input CLK4;
-
-    input [CFG_ABITS-1:0] A1ADDR;
-    output [CFG_DBITS-1:0] A1DATA;
-    input A1EN;
-
-    input [CFG_ABITS-1:0] B1ADDR;
-    input [CFG_DBITS-1:0] B1DATA;
-    input [CFG_ENABLE_B-1:0] B1EN;
-
-    input [CFG_ABITS-1:0] C1ADDR;
-    output [CFG_DBITS-1:0] C1DATA;
-    input C1EN;
-
-    input [CFG_ABITS-1:0] D1ADDR;
-    input [CFG_DBITS-1:0] D1DATA;
-    input [CFG_ENABLE_D-1:0] D1EN;
-
-    input [CFG_ABITS-1:0] E1ADDR;
-    output [CFG_DBITS-1:0] E1DATA;
-    input E1EN;
-
-    input [CFG_ABITS-1:0] F1ADDR;
-    input [CFG_DBITS-1:0] F1DATA;
-    input [CFG_ENABLE_F-1:0] F1EN;
-
-    input [CFG_ABITS-1:0] G1ADDR;
-    output [CFG_DBITS-1:0] G1DATA;
-    input G1EN;
-
-    input [CFG_ABITS-1:0] H1ADDR;
-    input [CFG_DBITS-1:0] H1DATA;
-    input [CFG_ENABLE_H-1:0] H1EN;
-
-endmodule
-`endcelldefine
-
-`celldefine
-(* blackbox *)
-module BRAM2x18_SDP (A1ADDR, A1DATA, A1EN, B1ADDR, B1DATA, B1EN, C1ADDR, C1DATA, C1EN, CLK1, CLK2, D1ADDR, D1DATA, D1EN);
-    parameter CFG_ABITS = 11;
-    parameter CFG_DBITS = 18;
-    parameter CFG_ENABLE_B = 4;
-    parameter CFG_ENABLE_D = 4;
-
-    parameter CLKPOL2 = 1;
-    parameter CLKPOL3 = 1;
-    parameter [18431:0] INIT0 = 18432'bx;
-    parameter [18431:0] INIT1 = 18432'bx;
-
-
-
-    input CLK1;
-    input CLK2;
-
-    input [CFG_ABITS-1:0] A1ADDR;
-    output [CFG_DBITS-1:0] A1DATA;
-    input A1EN;
-
-    input [CFG_ABITS-1:0] B1ADDR;
-    input [CFG_DBITS-1:0] B1DATA;
-    input [CFG_ENABLE_B-1:0] B1EN;
-
-    input [CFG_ABITS-1:0] C1ADDR;
-    output [CFG_DBITS-1:0] C1DATA;
-    input C1EN;
-
-    input [CFG_ABITS-1:0] D1ADDR;
-    input [CFG_DBITS-1:0] D1DATA;
-    input [CFG_ENABLE_D-1:0] D1EN;
-
-
-
-endmodule
-`endcelldefine
-
-`celldefine
-(* blackbox *)
-module \_$_mem_v2_asymmetric (RD_ADDR, RD_ARST, RD_CLK, RD_DATA, RD_EN, RD_SRST, WR_ADDR, WR_CLK, WR_DATA, WR_EN);
-
-    parameter CFG_ABITS = 10;
-    parameter CFG_DBITS = 36;
-    parameter CFG_ENABLE_B = 4;
-
-    parameter READ_ADDR_WIDTH = 11;
-    parameter READ_DATA_WIDTH = 16;
-    parameter WRITE_ADDR_WIDTH = 10;
-    parameter WRITE_DATA_WIDTH = 32;
-    parameter ABITS = 0;
-    parameter MEMID = 0;
-    parameter [36863:0] INIT = 36864'bx;
-    parameter OFFSET = 0;
-    parameter RD_ARST_VALUE = 0;
-    parameter RD_CE_OVER_SRST = 0;
-    parameter RD_CLK_ENABLE = 0;
-    parameter RD_CLK_POLARITY = 0;
-    parameter RD_COLLISION_X_MASK = 0;
-    parameter RD_INIT_VALUE = 0;
-    parameter RD_PORTS = 0;
-    parameter RD_SRST_VALUE = 0;
-    parameter RD_TRANSPARENCY_MASK = 0;
-    parameter RD_WIDE_CONTINUATION = 0;
-    parameter SIZE = 0;
-    parameter WIDTH = 0;
-    parameter WR_CLK_ENABLE = 0;
-    parameter WR_CLK_POLARITY = 0;
-    parameter WR_PORTS = 0;
-    parameter WR_PRIORITY_MASK = 0;
-    parameter WR_WIDE_CONTINUATION = 0;
-
-
-    input RD_CLK;
-    input WR_CLK;
-    input RD_ARST;
-    input RD_SRST;
-
-    input [CFG_ABITS-1:0] RD_ADDR;
-    output [CFG_DBITS-1:0] RD_DATA;
-    input RD_EN;
-
-    input [CFG_ABITS-1:0] WR_ADDR;
-    input [CFG_DBITS-1:0] WR_DATA;
-    input [CFG_ENABLE_B-1:0] WR_EN;
-
-  endmodule
 `endcelldefine
