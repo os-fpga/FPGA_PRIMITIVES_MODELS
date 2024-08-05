e62cf81475cb47979b128aa740a0b206202d0382
bilal458 <bilalahmed458@gmail.com>
2024-08-05 17:30:15 +0500

    Updated Directory structure

--- sim_models/verilog/I_BUF.v
+++ sim_models/verilog/I_BUF.v
@@ -9,6 +9,9 @@
 
 module I_BUF #(
       parameter WEAK_KEEPER = "NONE" // Specify Pull-up/Pull-down on input (NONE/PULLUP/PULLDOWN)
+`ifdef RAPIDSILICON_INTERNAL
+    ,  parameter IOSTANDARD = "DEFAULT" // IO Standard
+`endif // RAPIDSILICON_INTERNAL
 ) (
   input I, // Data input (connect to top-level port)
   input EN, // Enable the input
@@ -35,6 +38,40 @@ module I_BUF #(
       end
     endcase
 
+`ifdef RAPIDSILICON_INTERNAL
+
+    case(IOSTANDARD)
+      "DEFAULT" ,
+      "LVCMOS_12" ,
+      "LVCMOS_15" ,
+      "LVCMOS_18_HP" ,
+      "LVCMOS_18_HR" ,
+      "LVCMOS_25" ,
+      "LVCMOS_33" ,
+      "LVTTL" ,
+      "HSTL_I_12" ,
+      "HSTL_II_12" ,
+      "HSTL_I_15" ,
+      "HSTL_II_15" ,
+      "HSUL_12" ,
+      "PCI66" ,
+      "PCIX133" ,
+      "POD_12" ,
+      "SSTL_I_15" ,
+      "SSTL_II_15" ,
+      "SSTL_I_18_HP" ,
+      "SSTL_II_18_HP" ,
+      "SSTL_I_18_HR" ,
+      "SSTL_II_18_HR" ,
+      "SSTL_I_25" ,
+      "SSTL_II_25" ,
+      "SSTL_I_33" ,
+      "SSTL_II_33": begin end
+      default: begin
+        $fatal(1,"\nError: I_BUF instance %m has parameter IOSTANDARD set to %s.  Valid values are DEFAULT, LVCMOS_12, LVCMOS_15, LVCMOS_18_HP, LVCMOS_18_HR, LVCMOS_25, LVCMOS_33, LVTTL, HSTL_I_12, HSTL_II_12, HSTL_I_15, HSTL_II_15, HSUL_12, PCI66, PCIX133, POD_12, SSTL_I_15, SSTL_II_15, SSTL_I_18_HP, SSTL_II_18_HP, SSTL_I_18_HR, SSTL_II_18_HR, SSTL_I_25, SSTL_II_25, SSTL_I_33, SSTL_II_33\n", IOSTANDARD);
+      end
+    endcase
+`endif // RAPIDSILICON_INTERNAL
 
   end
 
