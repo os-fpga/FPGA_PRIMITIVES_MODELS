f765dc46a6433998924bd18674d833f59e7ddf1f
NadeemYaseen <pu.engineerg@gmail.com>
2024-08-05 10:58:03 +0000

    Added SIM change files from 1.4.7

--- sim_models/verilog/I_BUF.v
+++ sim_models/verilog/I_BUF.v
@@ -9,6 +9,7 @@
 
 module I_BUF #(
       parameter WEAK_KEEPER = "NONE" // Specify Pull-up/Pull-down on input (NONE/PULLUP/PULLDOWN)
+,  parameter IOSTANDARD = "DEFAULT" // IO Standard
 ) (
   input I, // Data input (connect to top-level port)
   input EN, // Enable the input
@@ -24,7 +25,10 @@ module I_BUF #(
 
   assign O = EN ? I : 1'b0;
 
-
+  specify
+    if (EN == 1'b1)
+    (I => O) = (0, 0);
+  endspecify
  initial begin
     case(WEAK_KEEPER)
       "NONE" ,
@@ -35,6 +39,37 @@ module I_BUF #(
       end
     endcase
 
+    case(IOSTANDARD)
+      "DEFAULT" ,
+      "LVCMOS_12" ,
+      "LVCMOS_15" ,
+      "LVCMOS_18_HP" ,
+      "LVCMOS_18_HR" ,
+      "LVCMOS_25" ,
+      "LVCMOS_33" ,
+      "LVTTL" ,
+      "HSTL_I_12" ,
+      "HSTL_II_12" ,
+      "HSTL_I_15" ,
+      "HSTL_II_15" ,
+      "HSUL_12" ,
+      "PCI66" ,
+      "PCIX133" ,
+      "POD_12" ,
+      "SSTL_I_15" ,
+      "SSTL_II_15" ,
+      "SSTL_I_18_HP" ,
+      "SSTL_II_18_HP" ,
+      "SSTL_I_18_HR" ,
+      "SSTL_II_18_HR" ,
+      "SSTL_I_25" ,
+      "SSTL_II_25" ,
+      "SSTL_I_33" ,
+      "SSTL_II_33": begin end
+      default: begin
+        $fatal(1,"\nError: I_BUF instance %m has parameter IOSTANDARD set to %s.  Valid values are DEFAULT, LVCMOS_12, LVCMOS_15, LVCMOS_18_HP, LVCMOS_18_HR, LVCMOS_25, LVCMOS_33, LVTTL, HSTL_I_12, HSTL_II_12, HSTL_I_15, HSTL_II_15, HSUL_12, PCI66, PCIX133, POD_12, SSTL_I_15, SSTL_II_15, SSTL_I_18_HP, SSTL_II_18_HP, SSTL_I_18_HR, SSTL_II_18_HR, SSTL_I_25, SSTL_II_25, SSTL_I_33, SSTL_II_33\n", IOSTANDARD);
+      end
+    endcase
 
   end
 
