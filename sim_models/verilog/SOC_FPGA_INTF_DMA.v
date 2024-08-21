935094e2cb9f385bc5c57cb603b9b737155af87a
NadeemYaseen <pu.engineerg@gmail.com>
2024-08-21 19:03:05 +0000

    Added SIM change files from 1.4.9

--- sim_models/verilog/SOC_FPGA_INTF_DMA.v
+++ sim_models/verilog/SOC_FPGA_INTF_DMA.v
@@ -14,5 +14,18 @@ module SOC_FPGA_INTF_DMA (
   input DMA_RST_N // DMA reset
 );
 
+
+reg [3:0] dma_ack;
+assign DMA_ACK = dma_ack;
+
+always@(posedge DMA_CLK) begin
+  if(!DMA_RST_N) begin
+    dma_ack <= 4'b0;
+  end 
+  else begin
+    dma_ack <= DMA_REQ;
+  end
+end
+
 endmodule
 `endcelldefine
